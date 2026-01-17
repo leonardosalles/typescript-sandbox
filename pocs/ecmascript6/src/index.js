b3d7673e91b67fd29329fd6b6cebb876aa34513e
leonardosalles <leosallestm@gmail.com>
2026-01-16 21:34:07 -0300

    Add tests to executor

--- pocs/ecmascript6/src/index.js
+++ pocs/ecmascript6/src/index.js
@@ -1,6 +1,9 @@
 import { createSmartUser } from './meta-programming.js';
 import { microtaskChallenge } from './async-deep.js';
 import { primeGenerator } from './iterators.js';
+import { BankAccount } from './private-weakmap.js';
+
+import './reflect.js';
 
 console.log("--- PROXY ---");
 const user = createSmartUser();
@@ -16,3 +19,11 @@ console.log(primes.next().value);
 console.log("\n--- EVENT LOOP ---");
 microtaskChallenge();
 
+const account = new BankAccount("Salles", 1000);
+console.log(account.balance);
+
+console.log("\n--- REFLECT API ---");
+
+
+runStressTest('Map');
+runStressTest('WeakMap');
